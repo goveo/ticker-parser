13db6ea568f39c1f0b87a86aefcbeae5512caf15
dr10n9 <dr10n9@gmail.com>
2018-10-29 23:49:34 +0000

    ticker module

--- src/database/ticker.js
+++ src/database/ticker.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 mongoose.Promise = global.Promise;
+const database = require('./database');
 
-const tickerSchema = new Schema("ticker", {
+const tickerSchema = new Schema({
     id: Number,
     link: String,
     name: String,
@@ -11,7 +12,7 @@ const tickerSchema = new Schema("ticker", {
     industry: String,
     country: String,
     marketCap: Number,
-    pe: Number, 
+    pe: Number,
     proce: Number,
     change: String,
     volume: Number
@@ -19,3 +20,50 @@ const tickerSchema = new Schema("ticker", {
 
 const Ticker = mongoose.model("ticker", tickerSchema);
 
+async function createTicker(tickerObject){
+    return await Ticker.create(new Ticker(tickerObject), (err, data) => {
+        if(err){
+            return err;
+        } else {
+            return "created";
+        }
+    });
+}
+
+async function getAllTickers(){
+    return await Ticker.find({}, (err, data) => {
+        if(err){
+            return err;
+        } else {
+            return data;
+        }
+    });
+}
+
+async function getTickerById(tickerId){
+    return await Ticker.findOne({id: tickerId}, (err, data) => {
+        if(err){
+            return err;
+        } else {
+            return data;
+        }
+    });
+}
+
+async function removeTickerById(tickerId){
+    return await Ticker.findOneAndDelete({id: tickerId}, (err, data) => {
+        if(err){
+            return err;
+        } else {
+            return "deleted";
+        }
+    });
+}
+
+module.exports = {
+    createTicker: createTicker,
+    getAllTickers: getAllTickers,
+    getTickerById: getTickerById,
+    removeTickerById: removeTickerById,
+    Ticker: Ticker
+}
