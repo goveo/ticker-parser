06bd55107225cfb8edb590706561cf9a89434df8
dr10n9 <dr10n9@gmail.com>
2018-10-30 11:59:29 +0000

    database start function

--- src/database/database.js
+++ src/database/database.js
@@ -1,10 +1,25 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise;
 const config = require('../config');
+const parser = require('../parser')
 
-mongoose.connect(config.database, (err, data) => {
-    if(err) console.log(err);
-    else{
-        console.log("mongo connected")
-    }
-});
+const URL = 'https://finviz.com/screener.ashx';
+
+
+async function start(){
+    await mongoose.connect(config.database, {
+        useNewUrlParser: true
+    }, (err, data) => {
+        if(err) console.log(err);
+        else{
+            console.log("mongo connected");
+            // setInterval(parser.parse(), );
+            parser.parse(URL);
+        }
+    });
+}
+
+module.exports = {
+    start: start
+}
+
